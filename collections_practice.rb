28109c4f35fa6d4d5333cabd67d2864696c4445a
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-17 23:23:47 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  
+  array.each do |x|
+    new_array << x[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all { |x| x[0] == "a" }
+end
+
+def sum_array(array)
+  sum = 0
+  array.each { |x| sum += x.to_i }
+  sum
+end
+
+def add_s(array)
+  array.map do |x|
+    if x == array[1]
+      x
+    else
+      x + "s"
+    end
+  end
+end
